110cab3f9a092cd1f2eafea676ae17cd90f4fa42
rushitv7265800 <107460866+rushitv7265800@users.noreply.github.com>
2024-04-21 11:37:38 +0530

    chages game

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 
-export const baseURL = "http://192.168.29.244:5091/";
-export const adminBaseURL = " https://allinone.codderlab.com/";
+export const baseURL = "https://aviator-backend-945q.onrender.com";
+export const adminBaseURL = " https://aviator-backend-945q.onrender.com";
 export const key = "vguikkOUno8Xcfvjhkiyb06aIKrejZ9R4h";
  const queryParams = new URLSearchParams(window.location.search);
-export const userId = queryParams.get("id");
+export const userId =  queryParams.get("id") ? queryParams.get("id") : "6527da15bdfb0b7c39fd700f";
